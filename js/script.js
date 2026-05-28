07bf43a7d2bb122d7a2971d47cf9f222f8f1689e
PauloRegisss <paulocesarregis@hotmail.com>
2026-05-28 11:21:48 -0300

    fix script

--- js/script.js
+++ js/script.js
@@ -20,22 +20,40 @@ if (APIResponse.status === 200) {
 
 const renderPokemon = async (pokemon) => {
 
-pokemonName.innerHTML = 'Loading...';
-pokemonNumber.innerHTML = '';
-    
-const data = await fetchPokemon(pokemon);
-    
-if (data) {
-    pokemonImage.style.display = 'block';
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value = '';
-    searchPokemon =data.id;
-}  else {
-    pokemonImage.style.display = 'none';        
-    pokemonName.innerHTML = 'Not found :c'; 
-    pokemonNumber.innerHTML = '';   
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+
+        pokemonImage.style.display = 'block';
+
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+
+        const animatedSprite =
+        data.sprites.versions['generation-v']['black-white'].animated.front_default;
+
+        const officialArtwork =
+        data.sprites.other['official-artwork'].front_default;
+
+        const defaultSprite =
+        data.sprites.front_default;
+
+        pokemonImage.src =
+        animatedSprite || officialArtwork || defaultSprite;
+
+        input.value = '';
+        searchPokemon = data.id;
+
+    } else {
+
+        pokemonImage.style.display = 'none';
+
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonNumber.innerHTML = '';
+
     }
 }
 
